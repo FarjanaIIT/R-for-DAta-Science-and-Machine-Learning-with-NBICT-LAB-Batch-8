9168d327c92e1ad3ab8673fdc331cdb26598ea2d
FarjanaIIT <farjana-bmb@sust.edu>
2025-08-11 16:09:25 +0600

    update visualize binomial

--- RDSML-Day-29 👉 Visualizing Binomial Probability Distribution/Visualiising binomial.R
+++ RDSML-Day-29 👉 Visualizing Binomial Probability Distribution/Visualiising binomial.R
@@ -16,6 +16,6 @@ visualize.binom(stat = 4, size = 10, prob = 0.5, section = "upper")
 visualize.binom(stat = c(3,7), size = 10, prob = 0.5, section = "bounded") 
 
 visualize.binom(stat = 4, size = 10, prob = 0.5)                
-grid(nx = NULL, ny = NULL, col = "grey", lty = "dottted")
+grid(nx = NULL, ny = NULL, col = "grey", lty = "dotted")
 
 
